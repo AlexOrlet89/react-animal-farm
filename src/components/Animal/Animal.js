b9e30bd9ff33466ff32da39fec2388e30869ea3a
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-07 15:21:28 -0800

    animals rendering

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './Animal.css';
 
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return <div className='animal' style={{ top: props.top, left: props.left }}>
+    <img alt={`${props.name}`} src={`./animals/${props.type}.svg`}></img>
+    <p className='name'>{`${props.name}`}</p>
+    <p>{`${props.says}`}</p>
+  </div>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Main.css';
+import Animal from '../Animal/Animal';
+
+import { animals } from '../../data';
 
 export default function Main() {
-  return <main></main>;
+  return <main className='main'>   
+    {animals.map((animal) => (
+      <Animal key={animal.name} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
+    ))}
+  </main>;
 }
