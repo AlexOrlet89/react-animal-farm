66dd4ad7f60f3508f0146eb6c15332e1c327bfb0
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-07 16:43:28 -0800

    moved bg image to main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Main.css';
 import Animal from '../Animal/Animal';
+import background from './background.png';
 
 import { animals } from '../../data';
 
 export default function Main() {
-  return <main className='main'>   
+  return <main className='main' style={{ backgroundImage: `url(${background})` }}>   
     {animals.map((animal) => (
       <Animal key={animal.name} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
     ))}
